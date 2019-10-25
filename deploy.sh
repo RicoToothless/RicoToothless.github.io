fe532a0966c204c70b853e7d686b3db84a9a90d3
rico <rico@bincentive.com>
2019-10-25 20:50:03 +0800

    fix deploy workflow

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-set -x
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # if fail would stop script
 set -e
-
-# Add changes to git.
-git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
 
 # Build the project.
 hugo -t inkblotty # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -36,3 +27,14 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+---
+# Go back to Hugo Project
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
